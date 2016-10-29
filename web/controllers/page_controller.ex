e4c86e8dfeff76144347602f1b5d74d06167e51f
llxff <ll.wg.bin@gmail.com>
2016-10-29 11:49:07 +0300

    do

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -7,12 +7,12 @@ defmodule UberHistory.PageController do
     client = conn.assigns[:oauth_client]
     current_user = Api.me(client)
 
-    _index(conn, client, current_user)
+    do_index(conn, client, current_user)
   end
 
-  defp _index(conn, _client, nil), do: render(conn, "index.html", current_user: nil)
+  defp do_index(conn, _client, nil), do: render(conn, "index.html", current_user: nil)
 
-  defp _index(conn, client, current_user) do
+  defp do_index(conn, client, current_user) do
     history = Api.history(client)
 
     render conn, "index.html", current_user: current_user, history: history
