ed3ec0719b8d38e82aa01bc27686c0d0392ae6b8
Александр Фомин <ll.wg.bin@gmail.com>
2016-11-11 22:46:37 +0300

    alias

--- web/services/uber/api.ex
+++ web/services/uber/api.ex
@@ -1,16 +1,18 @@
 defmodule Uber.Api do
+  alias OAuth2.{Client, Response}
+
   def me(client) do
-    OAuth2.Client.get!(client, "/v1/me") |> response
+    Client.get!(client, "/v1/me") |> response
   end
 
   def history(client, offset \\ 0, limit \\ 14) do
-    OAuth2.Client.get!(client, "/v1.2/history?offset=#{ offset }&limit=#{ limit }") |> response
+    Client.get!(client, "/v1.2/history?offset=#{ offset }&limit=#{ limit }") |> response
   end
 
   def receipt(client, request_id) do
-    OAuth2.Client.get!(client, "/v1/requests/#{ request_id }/receipt") |> response
+    Client.get!(client, "/v1/requests/#{ request_id }/receipt") |> response
   end
 
-  defp response(%OAuth2.Response{body: %{"code" => "unauthorized"}}), do: nil
-  defp response(%OAuth2.Response{body: body}), do: body
+  defp response(%Response{body: %{"code" => "unauthorized"}}), do: nil
+  defp response(%Response{body: body}), do: body
 end
