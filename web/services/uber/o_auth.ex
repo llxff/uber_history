9cde922655611f8f6f46067cb58705511c149bce
Александр Фомин <ll.wg.bin@gmail.com>
2016-11-14 20:27:24 +0300

    aliases

--- web/services/uber/o_auth.ex
+++ web/services/uber/o_auth.ex
@@ -1,5 +1,7 @@
 defmodule Uber.OAuth do
-  use OAuth2.Strategy
+  alias OAuth2.{Strategy, Strategy.AuthCode, Client}
+
+  use Strategy
 
   def config do
     [
@@ -14,29 +16,29 @@ defmodule Uber.OAuth do
   end
 
   def client do
-    OAuth2.Client.new(config)
+    Client.new(config)
   end
 
   def client(token) do
-    OAuth2.Client.new(config ++ [token: token])
+    Client.new(config ++ [token: token])
   end
 
   def authorize_url! do
-    OAuth2.Client.authorize_url!(client(), scope: Application.get_env(:uber, :scope))
+    Client.authorize_url!(client(), scope: Application.get_env(:uber, :scope))
   end
 
   def get_token!(params \\ [], headers \\ [], opts \\ []) do
-    OAuth2.Client.get_token!(client(), params, headers, opts)
+    Client.get_token!(client(), params, headers, opts)
   end
 
   def authorize_url(client, params) do
-    OAuth2.Strategy.AuthCode.authorize_url(client, params)
+    AuthCode.authorize_url(client, params)
   end
 
   def get_token(client, params, headers) do
     client
     |> put_param(:client_secret, client.client_secret)
     |> put_header("accept", "application/json")
-    |> OAuth2.Strategy.AuthCode.get_token(params, headers)
+    |> AuthCode.get_token(params, headers)
   end
 end
