17f67d31531b414e5613525953df71fb8a7cd3a5
Александр Фомин <ll.wg.bin@gmail.com>
2016-11-25 23:19:38 +0300

    fix namespace

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :uber_history, HelloPhoenix.Endpoint,
+config :uber_history, UberHistory.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "wiserider.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
@@ -9,7 +9,7 @@ config :uber_history, HelloPhoenix.Endpoint,
 
 config :logger, level: :info
 
-config :uber_history, HelloPhoenix.Repo,
+config :uber_history, UberHistory.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
