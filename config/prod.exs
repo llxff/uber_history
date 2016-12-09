6b8b04b1a2d878fb6cc775236d801cd4532f6185
Александр Фомин <ll.wg.bin@gmail.com>
2016-12-09 23:30:07 +0300

    prod domain

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,8 @@ use Mix.Config
 
 config :uber_history, UberHistory.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "wiserider.ru"],
+  url: [scheme: "https", host: "wiserider.ru", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
@@ -18,7 +19,7 @@ config :uber,
   client_id: System.get_env("UBER_CLIENT_ID"),
   client_secret: System.get_env("UBER_CLIENT_SECRET"),
   scope: "profile history request_receipt",
-  redirect_uri: "https://wiserider.herokuapp.com/auth/callback",
+  redirect_uri: "https://wiserider.ru/auth/callback",
   site: "https://sandbox-api.uber.com",
   authorize_url: "https://login.uber.com/oauth/v2/authorize",
   token_url: "https://login.uber.com/oauth/v2/token"
