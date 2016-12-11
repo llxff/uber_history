ee4f1d54de5b384de9d53d9909506bf80a9ae227
Александр Фомин <ll.wg.bin@gmail.com>
2016-12-11 23:30:48 +0300

    cp action

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule UberHistory.PageController do
   def index(conn, _params) do
     render conn, "index.html", token: get_session(conn, :access_token)
   end
+
+  def cp(conn, _params) do
+    render conn, "cp.html"
+  end
 end
